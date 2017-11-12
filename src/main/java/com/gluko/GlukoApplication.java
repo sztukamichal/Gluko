8c44777559cb1d04370d782caea10c43aaaee468
Michał <sztuuka.michal@gmail.com>
2017-11-12 17:58:07 +0100

    Initial commit - hello world

---
+++ src/main/java/com/gluko/GlukoApplication.java
@@ -0,0 +1,12 @@
+package com.gluko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlukoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlukoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gluko/GlukoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gluko;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GlukoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
