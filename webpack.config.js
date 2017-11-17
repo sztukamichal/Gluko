f2ea4142f1f1fbe828048c6454e1da33b3d0486d
Michał <sztuuka.michal@gmail.com>
2017-11-17 18:52:29 +0100

    almost everything configured - css classes not loading properly.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/main/app/src/index.js',
+    entry: './src/main/app/src/index.jsx',
     output: {
-        //path: path.resolve('./src/main/resources/static/built'),
         path: path.resolve('./target/classes/static/built'),
         filename: 'bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+            {test: /\.css$/, loader: 'style-loader', exclude: /node_modules/},
+            {test: /\.css$/, loader: 'css-loader', exclude: /node_modules/, query: {modules: true}},
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+            {test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/},
+            {test: /\.svg/, loader: 'svg-react-loader', exclude: /node_modules/}
         ]
     }
 };
