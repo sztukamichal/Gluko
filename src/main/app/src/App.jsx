b08daafb3dfb8c218aa6820ffeab1a77bccf045a
Michał <sztuuka.michal@gmail.com>
2017-11-18 16:19:34 +0100

    Task #40 - Dodanie Routera w React - router working

--- src/main/app/src/App.jsx
+++ src/main/app/src/App.jsx
@@ -1,20 +1,22 @@
 import React, {Component} from "react";
+import {Route, Switch} from "react-router-dom";
 import styles from "./App.css";
+import HelloRouter from "./components/HelloRouter/HelloRouter.jsx";
 
 class App extends Component {
-    render() {
-        return (
-            <div className={styles.container}>
-                <header className={styles.header}>
-                    <img src={"logo.svg"} className={styles.logo} alt="logo"/>
-                    <h1 className={styles.title}>Welcome to React !</h1>
-                </header>
-                <p className={styles.intro}>
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className={styles.container}>
+				<header className={styles.header}>
+					<img src={"logo.svg"} className={styles.logo} alt="logo"/>
+					<h1 className={styles.title}>Welcome to React !</h1>
+				</header>
+				<Switch>
+					<Route path="/hello" component={HelloRouter}/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/main/java/com/gluko/controller/HomeController.java
+++ src/main/java/com/gluko/controller/HomeController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "index";
-    }
-
+	
+	@RequestMapping(value = {"/", "/hello"})
+	public String index() {
+		return "index";
+	}
+	
 }
